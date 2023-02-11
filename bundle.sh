64d2b909d6159e6f043dd1e02a5aa2defad15790
tonyz74 <73653713+tonyz74@users.noreply.github.com>
2023-02-11 20:17:13 +0800

    Updated app bundler

--- bundle.sh
+++ bundle.sh
@@ -11,6 +11,6 @@ mkdir dist/$PROJECT.app/Contents/MacOS
 mkdir dist/$PROJECT.app/Contents/Resources
 
 cp -r assets dist/$PROJECT.app/Contents/MacOS
-cp target/x86_64-apple-darwin/release/shroom_boom dist/$PROJECT.app/Contents/MacOS/ShroomBoom
-cp dist/Info.plist dist/$PROJECT.app/Contents/Info.plist
-cp assets/icon/Icon.icns dist/$PROJECT.app/Contents/Resources/Icon.icns
+cp target/x86_64-apple-darwin/release/shroom_boom dist/$PROJECT.app/Contents/MacOS
+cp dist/Info.plist dist/$PROJECT.app/Contents
+cp assets/icon/Icon.icns dist/$PROJECT.app/Contents/Resources
